d8ebd6199ac31929136feaa7ae6c6ccd3dce84df
D0588593 <mariokart761@gmail.com>
2018-12-18 00:49:29 +0800

    Delete main.h

--- src/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
-
-int addNumber(int a, int b);
-int getTriangleType(int a, int b, int c);
-int Add4Num (int a,int b,int c,int d);
-int abc(int w );
-
-#endif // MAIN_H_INCLUDED
